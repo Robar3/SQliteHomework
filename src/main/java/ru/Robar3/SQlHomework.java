1de38477a9a8a07420a78a6fbae9732eb113ca10
Robar3 <terehov94@mail.ru>
2019-12-26 22:19:58 +0300

    Create db and output to console

--- src/main/java/ru/Robar3/SQlHomework.java
+++ src/main/java/ru/Robar3/SQlHomework.java
@@ -2,14 +2,25 @@ package ru.Robar3;
 
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class SQlHomework {
     Connection connection;
+    int a;
 
     public static void main(String[] args) {
         SQlHomework sQlHomework = new SQlHomework();
         if (sQlHomework.open()) {
-            sQlHomework.select();
+            System.out.println("Если нужно внести данные в таблицу введите 1," +
+                    "если нужно показать данные введите 2");
+            switch (sQlHomework.choice()) {
+                case (1):
+                    sQlHomework.insert();
+                    break;
+                case (2):
+                    sQlHomework.select();
+                    break;
+            }
             sQlHomework.end();
         }
     }
@@ -26,6 +37,29 @@ public class SQlHomework {
         }
     }
 
+    public int choice() {
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        return a;
+    }
+
+    public void insert() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter name");
+            String name = scanner.nextLine();
+            System.out.println("Enter age");
+            int age = scanner.nextInt();
+
+            String query = "INSERT INTO students (name,age) VALUES ('" + name + "','" + age + "')";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Rows add");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void select() {
         try {
             Statement statement = connection.createStatement();
@@ -35,16 +69,16 @@ public class SQlHomework {
             int column = resultSet.getMetaData().getColumnCount();
             String tableName = resultSet.getMetaData().getTableName(column);
             System.out.println("| " + tableName + " |");
-            for (int a = 1; a <= column-1; a++) {
+            for (int a = 1; a <= column - 1; a++) {
                 System.out.print("|" + resultSet.getMetaData().getColumnName(a) + "\t");
             }
-            System.out.printf("%8s",("|"+resultSet.getMetaData().getColumnName(3)));
+            System.out.printf("%8s", ("|" + resultSet.getMetaData().getColumnName(3)));
             System.out.println();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
                 int age = resultSet.getInt("age");
-                System.out.printf("|%d \t|%-7s \t|%d\n",id,name,age);
+                System.out.printf("|%d \t|%-7s \t|%d\n", id, name, age);
             }
             resultSet.close();
             statement.close();
